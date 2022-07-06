73c15cab3dea4aee56dbfe0b391946c9d2ba5f6d
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-07-06 11:00:18 +0100

    first commit

---
+++ createInventory.sh
@@ -0,0 +1,2 @@
+echo [ec2Instances] > inventory; 
+aws ec2 describe-instances    --query 'Reservations[*].Instances[*].PublicIpAddress'    --output text >> inventory;
---
+++ protectkey.sh
@@ -0,0 +1,1 @@
+chmod 600 ansi_key.pem
